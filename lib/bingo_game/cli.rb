801fac365aef2fc945698d34506027690c88fc42
ganmacs <ganmacs@gmail.com>
2015-12-12 03:01:07 +0900

    use roulette visual

--- lib/bingo_game/cli.rb
+++ lib/bingo_game/cli.rb
@@ -1,3 +1,4 @@
+require 'thread'
 require 'bingo_game/seed_generator'
 require 'bingo_game/flipper'
 
@@ -13,11 +14,30 @@ module BingoGame
         seed_generator.generate
         puts 'Success'
       else
+        shutto_suruyatu
         flipper.call
       end
     end
 
     private
+
+    def shutto_suruyatu
+      t = Thread.start {
+        loop { random_number }
+      }
+
+      gets.chomp                # stop roulette by any key
+      Thread.kill(t)
+      printf "\e[2A"
+    end
+
+    def random_number
+      [*0..90].shuffle.each do |num|
+        puts "#{num}"
+        sleep 0.05
+        printf "\e[1A"
+      end
+    end
 
     def seed_generator
       @seed_generator ||= SeedGenerator.new
